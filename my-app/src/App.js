81e391a790729beee869bf6f15641f7c58b546d4
nishimura-yurika <nishimura-yurika@ipl-soft.co.jp>
2022-04-19 13:55:45 +0900

    object-and-useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 
 const App = props => {
   const[state, setState] = useState(props)
+  const{name,price} = state
 
   return (
     <>
-    <p>現在の{state.name}は、{state.price}円です。</p>
-    <button onClick={() => setState({...state,price: state.price+1})}>+1</button>
-    <button onClick={() => setState({...state,price: state.price-1})}>-1</button>
+    <p>現在の{name}は、{price}円です。</p>
+    <button onClick={() => setState({...state,price: price+1})}>+1</button>
+    <button onClick={() => setState({...state,price: price-1})}>-1</button>
     <button onClick={() => setState(props)}>Reset</button>
-    <input value={state.name} onChange={e => setState({...state, name: e.target.value})}/>
+    <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
     </>
   )
 }
