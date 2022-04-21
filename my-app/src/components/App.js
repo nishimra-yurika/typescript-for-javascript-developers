c40dbb3cdc851bf959b36b1c54bb96a9a7975d04
nishimura-yurika <nishimura-yurika@ipl-soft.co.jp>
2022-04-21 17:00:56 +0900

    create operationLogs reducer

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,13 +1,14 @@
 import React,{ useReducer } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reducer from '../reducers/';
+import reducer from '../reducers';
 import EventForm from "./EventForm";
 import Events from "./Events";
 import AppContext from "../contexts/AppContext";
 
 const App = () => {
   const initialState = {
-    events: []
+    events: [],
+    operationLogs: []
   }
   const [state,dispatch] = useReducer(reducer,initialState)
   return (
